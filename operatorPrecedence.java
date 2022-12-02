0046294cb8e5da639469ffc1ea6982d5bb8d2189
Ganesh7625 <47949728+Ganesh7625@users.noreply.github.com>
2022-12-02 23:40:41 +0530

    Operators in JAVA

---
+++ operatorPrecedence.java
@@ -0,0 +1,15 @@
+public class operatorPrecedence {
+    public static void main(String[] args) {
+
+        System.out.println("Unary (Positive + ), (Negative - ), (Logical Not ! )");
+        System.out.println("(Multiplicative * )");
+        System.out.println("(Addtive + - )");
+        System.out.println("(Relational < > <= >= )");
+        System.out.println("(Eqality ==  !=  <> )");
+        System.out.println("(Logical AND && )");
+        System.out.println("(Logical OR || )");
+        System.out.println("(Logical XOR ^ )");
+        System.out.println("(Assignment -> )");
+    }
+
+}
