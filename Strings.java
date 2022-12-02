9d893e1f7ccedb7590bd9b4d6f4f43f58b889b28
Ganesh7625 <47949728+Ganesh7625@users.noreply.github.com>
2022-12-02 21:49:46 +0530

    String

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String[] args) {
+        System.out.println("This is _____ Statement");
+
+        String space = "blank"; // define a Strings
+
+        System.out.println("This is a new " + space + " Statement"); // Add strings into your code
+    }
+}
