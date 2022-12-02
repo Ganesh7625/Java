2dc53a001131c2e495f8e2a15a1b643ee35bc1b0
Ganesh7625 <47949728+Ganesh7625@users.noreply.github.com>
2022-12-02 23:04:33 +0530

    Data Types in Java

---
+++ dataTypes.java
@@ -0,0 +1,18 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        int Int = 9; // Interger/Numbers
+        long Long = 99999999L; // numbers long values
+        float Float = 2.5f; // Decimal values
+        double Double = 3.78; // Decimal long Values
+        char Character = 'a'; // Charaters single char
+        String str = "Something"; // strings
+
+        System.out.println(Int);
+        System.out.println(Long);
+        System.out.println(Float);
+        System.out.println(Double);
+        System.out.println(Character);
+        System.out.println(str);
+
+    }
+}
