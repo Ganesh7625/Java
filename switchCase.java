98b9c157082551411ceca9f6fa73c14c0ccec199
Ganesh7625 <47949728+Ganesh7625@users.noreply.github.com>
2022-12-04 21:41:49 +0530

    Switch Case

---
+++ switchCase.java
@@ -0,0 +1,20 @@
+public class switchCase {
+    public static void main(String[] args) {
+        int num = 10;
+
+        switch (num) {
+            case 10: {
+                System.out.println("Number is = 10");
+            }
+            case 20: {
+                System.out.println("Number is = 20");
+            }
+            case 30: {
+                System.out.println("Number is = 30");
+            }
+            default: {
+                System.out.println("Number is = 00");
+            }
+        }
+    }
+}
